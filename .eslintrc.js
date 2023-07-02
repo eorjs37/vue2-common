d2256323f709cde81faf1497e10aa093ab76fc0c
CHOI DAE GEON <cdg@aplayz.co.kr>
2023-07-03 08:12:29 +0900

    second commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "node": true
+  },
+  "parserOptions": {
+    "parser": "@babel/eslint-parser",
+    "sourceType": "module"
+  },
+  "extends": [
+    "plugin:vue/essential",
+    "eslint:recommended",
+  ],
+  "rules": {
+    "comma-dangle": "off",
+    "indent": ["warn", 2],
+    "no-nested-ternary": "warn",
+  }
+}
