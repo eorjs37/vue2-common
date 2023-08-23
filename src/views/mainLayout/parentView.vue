29f8cf3b86956679f50f9327d7a7781ff13d60f9
CHOI DAE GEON <cdg@aplayz.co.kr>
2023-08-24 07:07:01 +0900

    자식 컴퍼넌트 emit시키기

---
+++ src/views/mainLayout/parentView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Parent</h1>
+    <child-component @callemit="onCallEmit" />
+    <button @click="onCallEmit(456)">버튼</button>
+  </div>
+</template>
+<script>
+import ChildComponent from '@/components/ChildComponent.vue';
+export default {
+  name: 'ParentView',
+  components: {
+    'child-component': ChildComponent
+  },
+  data() {
+    return {
+      close: false
+    };
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+    onCallEmit(val) {
+      console.log(val);
+      this.close = true;
+    }
+  },
+};
+</script>
+<style scoped></style>
