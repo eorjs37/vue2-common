f309a6b37bafcef0ce50ecbd673813af5f8e62c9
CHOI DAE GEON <cdg@aplayz.co.kr>
2023-08-27 09:20:47 +0900

    클릭 추가

---
+++ src/views/mainLayout/createEleView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'CreateEleView',
+
+  data() {
+    return {
+
+    };
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+    testMethod() {
+      const newAtag = document.createElement('a');
+      newAtag.href = "https://aplayz.co.kr";
+      newAtag.click();
+    }
+  },
+};
+</script>
+<style scoped></style>
