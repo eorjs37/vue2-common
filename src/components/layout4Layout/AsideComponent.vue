093524e0e78d86686f001f347d21294621d83567
CHOI DAE GEON <cdg@aplayz.co.kr>
2023-09-04 08:33:15 +0900

    fix : 사이드바

--- src/components/layout4Layout/AsideComponent.vue
+++ src/components/layout4Layout/AsideComponent.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-
-  </div>
+  <aside class="side_bar on fixed h_100">
+    <nav class="h_100">
+      <h2>리스트</h2>
+      <ul>
+        <li>
+          1
+        </li>
+        <li>
+          2
+        </li>
+        <li>
+          3
+        </li>
+      </ul>
+    </nav>
+  </aside>
 </template>
 <script>
 export default {
@@ -22,4 +35,18 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.side_bar {
+  top: 0;
+  right: 0;
+  display: inline-block;
+  width: 200px;
+  transform: translateX(200px);
+  z-index: 1;
+  background-color: var(--black151515);
+}
+
+.side_bar.on {
+  transform: translateX(0);
+}
+</style>
--- src/layout/layout4Layout.vue
+++ src/layout/layout4Layout.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <layout4-header />
+    <aside-component />
     <main class="layout4_main">
       <router-view />
     </main>
   </div>
 </template>
 <script>
-import Layout4Header from '@/components/layout4Layout/Layout4Header.vue';
+import Layout4Header from '@/components/layout4Layout/Layout4HeaderComponent.vue';
+import AsideCompont from '@/components/layout4Layout/AsideComponent.vue';
 export default {
 
   name: 'Layout4Layout',
   components: {
-    'layout4-header': Layout4Header
+    'layout4-header': Layout4Header,
+    'aside-component': AsideCompont
   },
   data() {
     return {
