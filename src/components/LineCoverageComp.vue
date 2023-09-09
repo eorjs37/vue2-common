2225e448b92445f24d0d620ea64bc1d098b87e24
macdaegeon <chleorjs37@gmail.com>
2023-09-09 09:52:42 +0900

    fix : vue jest report

---
+++ src/components/LineCoverageComp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{ reversedMsg }}
+    <button @click="test">button</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Test coverage'
+
+    }
+  },
+  computed: {
+    reversedMsg() {
+      return this.reverseString(this.msg)
+    }
+  },
+  methods: {
+    reverseString(str) {
+      return str.split('').reverse().join('')
+    },
+    test() {
+      console.log(1)
+    }
+  }
+}
+</script>
+<style scoped></style>
---
+++ src/components/tests/unit/linecoverage.spec.js
@@ -0,0 +1,7 @@
+import { shallowMount } from "@vue/test-utils";
+import LineCoverageComp from '@/components/LineCoverageComp.vue'
+describe('Line Coverage comp test', () => {
+  test('test1', () => {
+    shallowMount(LineCoverageComp)
+  });
+});
