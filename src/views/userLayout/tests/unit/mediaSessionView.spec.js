78602ffd83e2ea10a788ecb75d7f2604e5f554ed
CHOI DAE GEON <cdg@aplayz.co.kr>
2023-10-23 07:16:10 +0900

    axios 추가

--- src/views/userLayout/tests/unit/mediaSessionView.spec.js
+++ src/views/userLayout/tests/unit/mediaSessionView.spec.js
@@ -1,6 +1,7 @@
 import { createLocalVue, shallowMount } from "@vue/test-utils";
 import mediaSessionView from '../../mediaSessionView.vue'
 import Plugins from "@/plugins/Plugins";
+import axios from "axios";
 describe('mediaSession view testing', () => {
 
   let wrapper = null;
@@ -9,11 +10,19 @@ describe('mediaSession view testing', () => {
     const localVue = createLocalVue()
     localVue.use(Plugins)
 
+
+    axios.get = jest.fn().mockImplementation(() => Promise.resolve({
+      data: [
+        { id: 1, username: "daisy" }
+      ]
+    }))
     wrapper = shallowMount(mediaSessionView, {
       localVue
     })
   });
   test('created', async () => {
-    console.log(wrapper)
+
+    expect(axios.get).toBeCalled();
+    expect(axios.get.mock.calls[0][0]).toBe('/api/list');
   });
 });
