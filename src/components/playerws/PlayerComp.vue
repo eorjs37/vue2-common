9ebfbee1dd65876969ff9df0cf59ad73da3d3958
macdaegeon <chleorjs37@gmail.com>
2023-11-04 14:11:48 +0900

    fix : 플레이어 퍼블리싱 진행중

---
+++ src/components/playerws/PlayerComp.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="wsplayer_wrap">
+    <div class="wsplayer_head">
+      <span class="channel_title">채널</span>
+      <span class="channel_name">스타필드 고양</span>
+      <div class="equalizer">
+        <span class="rythm first"></span>
+        <span class="rythm second"></span>
+        <span class="rythm third"></span>
+        <span class="rythm fourth"></span>
+        <span class="rythm five"></span>
+      </div>
+    </div>
+    <div class="wsplayer_body">
+      <div class="wscurrent_music">
+        <img class="wscurrent_music_img" :src="require(`@/assets/images/wsplayer/album.jpg`)" alt="album.jpg">
+        <div class="wscurrent_music_info">
+          <h2 class="wscurrent_music_title">있어줘요</h2>
+          <h5 class="wscurrent_music_artist">지아</h5>
+          <ul class="wscurrent_music_func">
+            <li class="music_func_item">
+              <img class="music_func_item_img" :src="require(`@/assets/images/wsplayer/like_icon_off.svg`)"
+                alt="like_icon_off">
+            </li>
+            <li class="music_func_item">
+              <img class="music_func_item_img" :src="require(`@/assets/images/wsplayer/hate_icon_off.svg`)"
+                alt="like_icon_off">
+            </li>
+            <li class="music_func_item">
+              <img class="music_func_item_img" :src="require(`@/assets/images/wsplayer/icon_pc_comment_30.svg`)"
+                alt="like_icon_off">
+            </li>
+            <li class="music_func_item">
+              <img class="music_func_item_img" :src="require(`@/assets/images/wsplayer/icon_pc_report_30.svg`)"
+                alt="like_icon_off">
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+    <div class="wsplayer_footer">
+      <div class="ws_play_control">
+        <img :src="require(`@/assets/images/wsplayer/icon_play.svg`)" alt="icon_play">
+
+      </div>
+      <div class="volume_control">
+        <img :src="require(`@/assets/images/wsplayer/volumup.svg`)" alt="volumup.svg">
+        <input type="range">
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "PlayerComp"
+}
+</script>
+<style scoped src="@/assets/styles/playerws/wsplayer.css"></style>
--- src/views/hlsLayout/playerWS.vue
+++ src/views/hlsLayout/playerWS.vue
@@ -5,29 +5,25 @@
       <span class="bold">스타필드 고양</span> 에 <br>
       어울리는 음악을 재생 중 입니다.
     </h1>
-    <!--###################### Curation ######################-->
-    <ul class="curation">
-      <li class="curationall">
-        <img :src="require(`@/assets/images/player/curation_setting_on.svg`)" alt="curation_setting">
+    <!--###################### Box ######################-->
+    <ul class="ws_box">
+      <li class="ws_left_box">
+        <!--###################### Player ######################-->
+        <player-comp />
       </li>
-
-      <li class="curation_option">공간 분위기</li>
-      <li class="curation_line"></li>
-      <li class="curation_option">국내곡 비율</li>
-      <li class="curation_line"></li>
-      <li class="curation_option">선호 장르</li>
-      <li class="curation_line"></li>
-      <li class="curation_option">선호 시디</li>
-      <li class="curation_line"></li>
-      <li class="curation_option">선호 음악 분위기</li>
+      <li class="ws_right_box">
+        <!--###################### Recommend Channel ######################-->
+      </li>
     </ul>
-    <!--###################### Player Box ######################-->
   </div>
 </template>
 <script>
+import PlayerComp from '@/components/playerws/PlayerComp.vue'
 export default {
   name: 'PlayerWS',
-
+  components: {
+    'player-comp': PlayerComp
+  },
   data() {
     return {
 
