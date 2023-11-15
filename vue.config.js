95af372d0529d0333a15138ec4c2bcadd4df179d
CHOI DAE GEON <cdg@aplayz.co.kr>
2023-11-16 07:36:07 +0900

    웹팩 분석 추가

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
@@ -8,7 +9,8 @@ module.exports = defineConfig({
         org: "no-vx9",
         project: "vue2-editorconfig",
         authToken: "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"
-      })
+      }),
+      new BundleAnalyzerPlugin()
     ]
   },
   chainWebpack: (config) => {
