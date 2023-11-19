5279659868bf7161879bcc4188199ccaddd53fc2
macdaegeon <chleorjs37@gmail.com>
2023-11-19 12:20:42 +0900

    커밋

--- src/views/obigoLayout/obigoView.vue
+++ src/views/obigoLayout/obigoView.vue
@@ -1,32 +1,85 @@
 <template>
   <section class="obigo_container">
-    <swiper-container :speed="500" :loop="true" :centered-slides="true" :css-mode="true" :pagination="{
-      hideOnClick: true
-    }">
-      <swiper-slide>
-        Slide 1
+    <swiper class="swiper obigo_w100" :options="swiperOption">
+      <swiper-slide class="slide">
+        <div class="obigo_swiper">
+          <div class="obigo_car_music">
+            <div class="obigo_car_music_box">
+              <h1 class="obigo_music_title">Good Mood (feat. Terri Walker) Good</h1>
+              <h2 class="obigo_music_artist">Donae’o featuring Terri Walker</h2>
+              <ul class="obigo_mucis_control">
+                <li class="obigo_album_item_box">
+                  <figure class="obigo_thumb_wrap">
+                    <img class="obigo_album_img" :src="require('@/assets/images/obigo/thumb_active.png')"
+                      alt="thumb_active ">
+                  </figure>
+                </li>
+
+
+              </ul>
+            </div>
+            <div class="obigo_album_box">
+              <figure class="obigo_album_wrap">
+                <img class="obigo_album_img" :src="require('@/assets/images/obigo/photographer.jpeg')" alt="photographer">
+              </figure>
+
+            </div>
+          </div>
+          <img class="obigo_music_album_bgimg" :src="require('@/assets/images/obigo/photographer.jpeg')">
+        </div>
+
       </swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-    </swiper-container>
+      <swiper-slide class="slide">
+        <div class="obigo_swiper">
+          <div class="obigo_car_music">
+            <h1 class="title">Slide2</h1>
+          </div>
+          <img class="obigo_music_album_bgimg" :src="require('@/assets/images/obigo/photographer.jpeg')">
+        </div>
+      </swiper-slide>
+      <swiper-slide class="slide">
+        <div class="obigo_swiper">
+          <div class="obigo_car_music">
+            <h1 class="title">Slide3</h1>
+          </div>
+          <img class="obigo_music_album_bgimg" :src="require('@/assets/images/obigo/photographer.jpeg')">
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </section>
 </template>
 <script>
-import { register } from 'swiper/element/bundle';
-register();
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 export default {
   name: 'ObigoView',
-
+  components: {
+    Swiper,
+    SwiperSlide
+  },
   data() {
     return {
+      swiperOption: {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+      }
     };
   },
 
   mounted() {
-
+    document.addEventListener("touchstart", () => { }, true);
   },
 
   methods: {
+    onSlideChange() {
+      console.log('slide change');
+
+    }
   },
   computed: {
     backgroundImageInlineStyle() {
