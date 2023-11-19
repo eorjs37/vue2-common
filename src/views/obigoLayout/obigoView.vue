f0c34152c79bcc5af42f32973c57f7ad6bf7160e
macdaegeon <chleorjs37@gmail.com>
2023-11-19 18:55:41 +0900

    배열

--- src/views/obigoLayout/obigoView.vue
+++ src/views/obigoLayout/obigoView.vue
@@ -1,21 +1,16 @@
 <template>
   <section class="obigo_container">
-    <nav class="obigo_nav">
-      <ul>
-        <li>1</li>
-      </ul>
-    </nav>
     <!--obigo_swiper_nav_open-->
     <swiper class="swiper obigo_w100 " :options="swiperOption">
-      <swiper-slide class="slide">
+      <swiper-slide class="slide" v-for="(item) in channelList" :key="item.id">
         <div class="obigo_swiper">
           <div class="obigo_car_music">
             <div class="obigo_car_music_box">
-              <h1 class="obigo_music_title" data-swiper-parallax="-2300">Good Mood (feat. Terri Walker) Good</h1>
-              <h2 class="obigo_music_artist" data-swiper-parallax="-1200">Donae’o featuring Terri Walker</h2>
+              <h1 class="obigo_music_title" data-swiper-parallax="-2300">{{ item.musicInfo.musicTitle }}</h1>
+              <h2 class="obigo_music_artist" data-swiper-parallax="-1200">{{ item.musicInfo.musicArtist }}</h2>
               <ul class="obigo_mucis_control" data-swiper-parallax="-900">
                 <li class="align_center">
-                  <figure id="thumbup" class="obigo_album_item_box" @click="thumbActive">
+                  <figure id="thumbup" class="obigo_album_item_box">
                     <img class="obigo_thumb_wrap obigo_album_thumb"
                       :src="require('@/assets/images/obigo/thumb_active.png')" alt="thumb_active ">
                   </figure>
@@ -36,89 +31,16 @@
             </div>
             <div class="obigo_album_box" data-swiper-parallax="-1200">
               <figure class="obigo_album_wrap">
-                <img class="obigo_album_img" :src="require('@/assets/images/obigo/album.png')" alt="photographer">
+                <img class="obigo_album_img" :src="require(`@/assets/images/obigo/${item.albumImgUrl}`)"
+                  alt="photographer">
               </figure>
 
             </div>
           </div>
-          <img class="obigo_music_album_bgimg" :src="require('@/assets/images/obigo/album.png')">
-        </div>
-
-      </swiper-slide>
-      <swiper-slide class="slide">
-        <div class="obigo_swiper">
-          <div class="obigo_car_music">
-            <div class="obigo_car_music_box">
-              <h1 class="obigo_music_title" data-swiper-parallax="-2300">Good Mood (feat. Terri Walker) Good</h1>
-              <h2 class="obigo_music_artist" data-swiper-parallax="-900">Donae’o featuring Terri Walker</h2>
-              <ul class="obigo_mucis_control" data-swiper-parallax="-700">
-                <li class="align_center">
-                  <figure class="obigo_album_item_box" @click="thumbActive">
-                    <img class="obigo_thumb_wrap obigo_album_thumb"
-                      :src="require('@/assets/images/obigo/thumb_active.png')" alt="thumb_active ">
-                  </figure>
-                </li>
-                <li class="obigo_album_item_box_musicplay">
-                  <figure class="obigo_play_wrap">
-                    <img class="obigo_album_img" :src="require('@/assets/images/obigo/play.png')" alt="play">
-                  </figure>
-                </li>
-                <li class="obigi_music_control_item last align_center">
-                  <figure class="obigo_album_item_box">
-                    <img class="obigo_thumb_wrap obigo_album_thumb" :src="require('@/assets/images/obigo/thumb_down.png')"
-                      alt="thumb_down">
-                  </figure>
-                </li>
-
-              </ul>
-            </div>
-            <div class="obigo_album_box" data-swiper-parallax="-1200">
-              <figure class="obigo_album_wrap">
-                <img class="obigo_album_img" :src="require('@/assets/images/obigo/photographer.jpeg')" alt="photographer">
-              </figure>
-
-            </div>
-          </div>
-          <img class="obigo_music_album_bgimg" :src="require('@/assets/images/obigo/photographer.jpeg')">
+          <img class="obigo_music_album_bgimg" :src="require(`@/assets/images/obigo/${item.albumImgUrl}`)">
         </div>
       </swiper-slide>
-      <swiper-slide class="slide">
-        <div class="obigo_swiper">
-          <div class="obigo_car_music">
-            <div class="obigo_car_music_box">
-              <h1 class="obigo_music_title" data-swiper-parallax="-1200">Good Mood (feat. Terri Walker) Good</h1>
-              <h2 class="obigo_music_artist" data-swiper-parallax="-900">Donae’o featuring Terri Walker</h2>
-              <ul class="obigo_mucis_control" data-swiper-parallax="-700">
-                <li class="align_center">
-                  <figure class="obigo_album_item_box" @click="thumbActive">
-                    <img class="obigo_thumb_wrap obigo_album_thumb"
-                      :src="require('@/assets/images/obigo/thumb_active.png')" alt="thumb_active ">
-                  </figure>
-                </li>
-                <li class="obigo_album_item_box_musicplay">
-                  <figure class="obigo_play_wrap">
-                    <img class="obigo_album_img" :src="require('@/assets/images/obigo/play.png')" alt="play">
-                  </figure>
-                </li>
-                <li class="obigi_music_control_item last align_center">
-                  <figure class="obigo_album_item_box">
-                    <img class="obigo_thumb_wrap obigo_album_thumb" :src="require('@/assets/images/obigo/thumb_down.png')"
-                      alt="thumb_down">
-                  </figure>
-                </li>
 
-              </ul>
-            </div>
-            <div class="obigo_album_box" data-swiper-parallax="-1200">
-              <figure class="obigo_album_wrap">
-                <img class="obigo_album_img" :src="require('@/assets/images/obigo/photographer.jpeg')" alt="photographer">
-              </figure>
-
-            </div>
-          </div>
-          <img class="obigo_music_album_bgimg" :src="require('@/assets/images/obigo/photographer.jpeg')">
-        </div>
-      </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
   </section>
@@ -144,31 +66,83 @@ export default {
           el: '.swiper-pagination',
           clickable: true
         },
-      }
+      },
+      channelList: [
+        {
+          id: "channel1",
+          musicInfo: {
+            musicTitle: "Good Mood (feat. Terri Walker) Good",
+            musicArtist: "Donae’o featuring Terri Walker"
+          },
+          titleSpeed: -2300,
+          artistSpeed: -1200,
+          musicControlSpeed: -900,
+          albumBoxSppe: -1200,
+          albumImgUrl: 'album.png'
+        },
+        {
+          id: "channel2",
+          musicInfo: {
+            musicTitle: "Good Mood (feat. Terri Walker) Good",
+            musicArtist: "Donae’o featuring Terri Walker"
+          },
+          titleSpeed: -2300,
+          artistSpeed: -1200,
+          musicControlSpeed: -900,
+          albumBoxSppe: -1200,
+          albumImgUrl: 'photographer.jpeg'
+        },
+        {
+          id: "channel3",
+          musicInfo: {
+            musicTitle: "Good Mood (feat. Terri Walker) Good",
+            musicArtist: "Donae’o featuring Terri Walker"
+          },
+          titleSpeed: -2300,
+          artistSpeed: -1200,
+          musicControlSpeed: -900,
+          albumBoxSppe: -1200,
+          albumImgUrl: 'album.png'
+        }, {
+          id: "channel4",
+          musicInfo: {
+            musicTitle: "Good Mood (feat. Terri Walker) Good",
+            musicArtist: "Donae’o featuring Terri Walker"
+          },
+          titleSpeed: -2300,
+          artistSpeed: -1200,
+          musicControlSpeed: -900,
+          albumBoxSppe: -1200,
+          albumImgUrl: 'photographer.jpeg'
+        }, {
+          id: "channel5",
+          musicInfo: {
+            musicTitle: "Good Mood (feat. Terri Walker) Good",
+            musicArtist: "Donae’o featuring Terri Walker"
+          },
+          titleSpeed: -2300,
+          artistSpeed: -1200,
+          musicControlSpeed: -900,
+          albumBoxSppe: -1200,
+          albumImgUrl: 'album.png'
+        }
+      ]
     };
   },
 
   mounted() {
-    const thumbUpele = document.querySelector('#thumbup');
-    const thumbDownele = document.querySelector('#thumbdown');
-    thumbUpele.addEventListener("touchstart", (ele) => {
-      ele.target.classList.add('active');
-    })
-    thumbUpele.addEventListener("touchend", (ele) => {
-      ele.target.classList.remove('active');
-    })
-
-    thumbDownele.addEventListener("touchstart", (ele) => {
-      ele.target.classList.add('active');
-    })
-    thumbDownele.addEventListener("touchend", (ele) => {
-      ele.target.classList.remove('active');
-    })
   },
 
   methods: {
-    thumbActive() {
-
+    thumbActive($ele) {
+      if ($ele) {
+        $ele.target.classList.add('active');
+      }
+    },
+    thumbUnActive($ele) {
+      if ($ele) {
+        $ele.target.classList.remove('active');
+      }
     }
   },
   computed: {
