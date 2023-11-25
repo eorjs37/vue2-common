5489f014ba350e1673d557175b01a7f33075b9f5
macdaegeon <chleorjs37@gmail.com>
2023-11-25 10:17:56 +0900

    swiper 진행중

--- src/views/obigoLayout/obigoPurposeView.vue
+++ src/views/obigoLayout/obigoPurposeView.vue
@@ -1,81 +1,60 @@
 <template>
   <div class="obigo_container">
-    <swiper-container effect="creative" :modules="modules" :navigation="true" class="swiper-container" :loop="true"
-      :slides-per-view="2.5" :centered-slides="true" :creative-effect="{
-        prev: {
-          shadow: true,
-          origin: 'left center',
-          translate: ['-5%', 0, -200],
-          rotate: [0, 100, 0],
-        },
-        next: {
-          origin: 'right center',
-          translate: ['5%', 0, -200],
-          rotate: [0, -100, 0],
-        },
-      }" :grabCursor="true" @swiperprogress="onProgress" @swiperslidechange="onSlideChange">
-      <swiper-slide>
-        <figure class="slider-image">
-          <img class="slider-image_img " :src="require('@/assets/images/obigo/purpose/business.png')" alt="business">
-        </figure>
-      </swiper-slide>
-      <swiper-slide>
-        <figure class="slider-image">
-          <img class="slider-image_img " :src="require('@/assets/images/obigo/purpose/daily.png')" alt="daily">
-        </figure>
-      </swiper-slide>
-      <swiper-slide>
-        <figure class="slider-image">
-          <img class="slider-image_img " :src="require('@/assets/images/obigo/purpose/drive.png')" alt="drive">
-        </figure>
-      </swiper-slide>
-      <swiper-slide>
-        <figure class="slider-image">
-          <img class="slider-image_img " :src="require('@/assets/images/obigo/purpose/gotowork.png')" alt="gotowork">
-        </figure>
-      </swiper-slide>
-      <swiper-slide>
-        <figure class="slider-image">
-          <img class="slider-image_img " :src="require('@/assets/images/obigo/purpose/leavework.png')" alt="leavework">
-        </figure>
-      </swiper-slide>
-      <swiper-slide>
-        <figure class="slider-image">
-          <img class="slider-image_img " :src="require('@/assets/images/obigo/purpose/trip.png')" alt="trip">
-        </figure>
-      </swiper-slide>
-    </swiper-container>
+    <div ref="swiper" class="swiper">
+      <!-- Additional required wrapper -->
+      <div class="swiper-wrapper">
+        <!-- Slides -->
+        <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+      </div>
+
+      <!-- If we need navigation buttons -->
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+
+    </div>
   </div>
 </template>
 <script>
-import { register } from 'swiper/element/bundle';
-import { Navigation, EffectCoverflow, EffectFade, EffectCreative } from 'swiper/modules'
-register();
 
+import Swiper from 'swiper';
+import { Navigation, EffectCreative } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
 export default {
   name: 'ObigoPurposeView',
   data() {
     return {
-      modules: [Navigation, EffectCoverflow, EffectFade, EffectCreative],
-      coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      },
-      creativeEffect: {
-        prev: {
-          opacity: 0,
-        },
-        next: {
-          opacity: 0,
-        },
-      }
     };
   },
 
   mounted() {
+    new Swiper(this.$refs.swiper, {
+      modules: [Navigation, EffectCreative],
+      loop: true,
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      // effect: 'creative',
+      // creativeEffect: {
+      //   prev: {
+      //     shadow: true,
+      //     origin: 'left center',
+      //     translate: ['-5%', 0, -200],
+      //     rotate: [0, 100, 0],
+      //   },
+      //   next: {
+      //     origin: 'right center',
+      //     translate: ['5%', 0, -200],
+      //     rotate: [0, -100, 0],
+      //   },
+      // },
+      slidesPerView: 2.5
+    })
   },
   methods: {
     onProgress() { },
