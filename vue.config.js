5c40e52378335eba96e6c5819be3af58e66f70a3
CHOI DAE GEON <cdg@aplayz.co.kr>
2023-12-08 07:59:48 +0900

    빌드 최적화 진행중

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,26 @@
 const { defineConfig } = require('@vue/cli-service');
-const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+// const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
+//const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
+    target: 'web',
     plugins: [
-      sentryWebpackPlugin({
-        org: "no-vx9",
-        project: "vue2-editorconfig",
-        authToken: "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"
-      }),
-      new BundleAnalyzerPlugin()
+      //new BundleAnalyzerPlugin()
+      // sentryWebpackPlugin({
+      //   org: "no-vx9",
+      //   project: "vue2-editorconfig",
+      //   authToken: "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"
+      // }),
+      //
     ],
+    externals(_, request, callback) {
+      if (/vue-lottie/.test(request)) {
+        return callback(null, `commonjs ${request}`);
+      }
+      callback();
+    }
   },
   chainWebpack: (config) => {
     config.plugin('html').tap(args => {
