a3da655631644c3bcb90b81b1c459d0fd854b2f1
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-03-18 07:34:54 +0900

    fix : 자식 컴퍼넌트 테스트

---
+++ src/components/PlayerChildComp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="sendData">버튼</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Vue2EditorconfigPlayerChildComp',
+  emits: ['send-data'],
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {
+    sendData() {
+      this.$emit('send-data', '데이터를 전송합니다.')
+    }
+  }
+}
+</script>
