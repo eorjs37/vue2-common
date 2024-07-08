73a369446634d2044e809841f59fdf98a71954fc
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-07-09 08:32:23 +0900

    app.spec.js 수정

--- src/tests/unit/App.spec.js
+++ src/tests/unit/App.spec.js
@@ -1,5 +1,24 @@
+import App from '@/App.vue'
+import { shallowMount } from '@vue/test-utils'
+import { router } from '@/router'
 describe('App', () => {
   test('Dummy Rendering', async () => {
+    shallowMount(App, {
+      global: {
+        plugins: [router],
+        mocks: {
+          $crontab: {
+            addJob: jest.fn()
+          },
+          $VideoPlayer: {
+            setVideo: jest.fn(),
+            playerMusic: jest.fn(),
+            startLoad: jest.fn(),
+            onPlay: jest.fn().mockImplementation(() => Promise.resolve())
+          }
+        }
+      }
+    })
     expect(1).toBe(1)
   })
 })
