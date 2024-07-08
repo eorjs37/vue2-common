58b295f66bcd4db8f8f63b021c0bee29bb3df591
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-07-09 08:05:51 +0900

    버전업

--- jest.config.js
+++ jest.config.js
@@ -10,29 +10,33 @@ module.exports = {
     'ts',
     'tsx',
     'json',
-    'node',
+    'node'
   ],
   transform: {
     // process `*.vue` files with `vue-jest`
-    '.*\\.(vue)$': ['vue-jest',
+    '.*\\.(vue)$': [
+      '@vue/vue3-jest',
       {
-        'sourceMaps': true
-      }],
+        sourceMaps: true
+      }
+    ],
     // process `*.js` files with `babel-jest`
-    '.*\\.(js)$': ['babel-jest',
+    '.*\\.(js)$': [
+      'babel-jest',
       {
-        'sourceMaps': true
-      }]
+        sourceMaps: true
+      }
+    ]
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    'axios': 'axios/dist/node/axios.cjs',
+    axios: 'axios/dist/node/axios.cjs',
     '\\.(css|less)$': '<rootDir>/tests/jest/__mocks__/styleMock.js'
   },
   // The glob patterns Jest uses to detect test files
   testMatch: [
     '<rootDir>/**/*.spec.(js|jsx|ts|tsx)',
-    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
+    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
   ],
   collectCoverage: false,
   collectCoverageFrom: [
@@ -47,7 +51,8 @@ module.exports = {
     '!**/coverage/**'
   ],
   // '<rootDir>/node_modules/', '/node_modules/(?!@scu/vue)'
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(particles-bg-vue)|(?!@scu/vue)|(?!(vue-lottie))/)'],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!(particles-bg-vue)|(?!@scu/vue)|(?!(vue3-lottie))/)'
+  ],
   setupFiles: ['jest-canvas-mock']
-
 }
--- src/tests/unit/App.spec.js
+++ src/tests/unit/App.spec.js
@@ -1,8 +1,3 @@
-import { createLocalVue } from '@vue/test-utils'
-import VueRouter from 'vue-router'
-const localVue = createLocalVue()
-localVue.use(VueRouter)
-
 describe('App', () => {
   test('Dummy Rendering', async () => {
     expect(1).toBe(1)
