5a2da4575b073bba3ea920d77a6167c97045e664
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-09-03 07:39:32 +0900

    todo 버튼 누를 경우 할일 추가

--- src/components/todo/TodoInput.vue
+++ src/components/todo/TodoInput.vue
@@ -1,23 +1,42 @@
 <template>
   <div class="w_100 input_wrap">
-    <input id="input" class="input" @keypress.enter="inputEnter" type="text" />
-    <button class="addbtn">추가</button>
+    <input
+      id="input"
+      class="input"
+      v-model="inputValue"
+      @input="onInputEvent"
+      @keydown="inputEnter"
+      type="text"
+    />
+    <button id="addbtn" class="addbtn" @click="addTodo">추가</button>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
 // eslint-disable-next-line no-undef
 const emit = defineEmits(['inputtext'])
 
+const inputValue = ref('')
+
+const onInputEvent = ($event) => {
+  inputValue.value = $event.target.value
+}
+
 const inputEnter = ($event) => {
+  if ($event.isComposing) return true
   if ($event.key.toLowerCase() === 'enter') {
     if (!$event) return
-    const { target } = $event
-    const { value } = target
     $event.preventDefault()
-    emit('inputtext', value)
-    $event.target.value = ''
+    emit('inputtext', inputValue.value)
+    inputValue.value = ''
   }
+}
+
+const addTodo = () => {
+  emit('inputtext', inputValue.value)
+  inputValue.value = ''
 }
 </script>
 <style scoped>
--- src/components/todo/tests/unit/todoinput.spec.js
+++ src/components/todo/tests/unit/todoinput.spec.js
@@ -9,8 +9,16 @@ describe('TodoInputItem Unit Test', () => {
   test('값을 입력하고 enter를 누르면 emit("inputtext")를 호출한다', async () => {
     const input = wrapper.find('#input')
     input.setValue('오늘의 할일')
-    await input.trigger('keypress.enter')
+    await input.trigger('keydown.enter')
 
     expect(wrapper.emitted()['inputtext'][0]).toEqual(['오늘의 할일'])
+  })
+
+  test('추가버튼을 클릭하면 emit("inputtext")를 호출한다', async () => {
+    const addBtn = wrapper.find('#addbtn')
+    const input = wrapper.find('#input')
+    input.setValue('오늘의 할일1')
+    await addBtn.trigger('click')
+    expect(wrapper.emitted()['inputtext'][0]).toEqual(['오늘의 할일1'])
   })
 })
