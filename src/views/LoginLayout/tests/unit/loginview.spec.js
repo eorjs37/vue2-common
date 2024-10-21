7bf3c6dcd895891e885a5ee0dd5c3e5c3582a3d3
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-10-22 08:56:30 +0900

    Input 이벤트

--- src/views/LoginLayout/tests/unit/loginview.spec.js
+++ src/views/LoginLayout/tests/unit/loginview.spec.js
@@ -6,9 +6,25 @@ describe('loginview unit testing', () => {
     wrapper = shallowMount(LoginView)
   })
 
-  test('ID입력 후 엔터', async () => {
+  test('ID입력 input 값 변경 확인', async () => {
+    //given
     const idInput = wrapper.find('#id')
+
+    //when
     idInput.setValue('chleorjs37@gmail.com')
+
+    //then
     expect(wrapper.vm.id).toBe('chleorjs37@gmail.com')
+  })
+
+  test('비밀번호 입력 input 값 변경 확인', async () => {
+    //given
+    const passwordInput = wrapper.find('#password')
+
+    //when
+    passwordInput.setValue('1234')
+
+    //then
+    expect(wrapper.vm.password).toBe('1234')
   })
 })
